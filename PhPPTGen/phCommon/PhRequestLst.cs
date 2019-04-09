26cda8c016c2b44d856b3fac11660a030aa6ecb2
dcs <419787760@qq.com>
2019-04-09 17:02:49 +0800

    init

---
+++ PhPPTGen/phCommon/PhRequestLst.cs
@@ -0,0 +1,67 @@
+﻿using PhPPTGen.phModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PhPPTGen.phCommon {
+	public class PhRequestLst {
+		private Object locker = new Object();
+		private List<PhRequest> lst = new List<PhRequest>();
+		private bool isRunning = true;
+
+		private static PhRequestLst instance;
+		public static PhRequestLst GetInstance() {
+			if (instance == null) {
+				instance = new PhRequestLst();
+			}
+			return instance;
+		}
+
+		private PhRequestLst() {
+
+		}
+
+		public void StartChecking() {
+			Thread t = new Thread(new ThreadStart(this.CheckingHandler));
+			t.Start();
+			//t.Join();
+		}
+
+		public void StopChecking() {
+			this.isRunning = false;
+		}
+
+		public void CheckingHandler() {
+
+			while (isRunning) {
+				Thread.Sleep(500);
+				lock (locker) {
+					if (lst.Count > 0) {
+						List<PhRequest>.Enumerator iterator = lst.GetEnumerator();
+						if (iterator.MoveNext()) {
+							PhRequest req = iterator.Current;
+							Console.WriteLine("Current Command is :");
+							Console.WriteLine(req.command);
+							string cls = phModel.PhMsgDefine.PhCommand2Cls(req.command);
+							phCommandFactory.PhCommandFactory fct = phCommandFactory.PhCommandFactory.GetInstance();
+							fct.CreateCommandInstance(cls, req);
+							lst.Remove(req);
+						}
+					}
+				}
+			}
+
+			StopChecking();
+		}
+
+		public void PushMsg(PhRequest req) {
+			lock (locker) {
+				lst.Add(req);
+				Console.WriteLine(lst.Count);
+			}
+		}
+	}
+}

908f43eb11d92446df65c9ab41510ad46554ddaf
dcs <419787760@qq.com>
2019-05-09 19:52:20 +0800

    本地测试能生成

--- PhPPTGen/Program.cs
+++ PhPPTGen/Program.cs
@@ -13,46 +13,51 @@ using System.IO;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using Spire.Xls;
+using PhPPTGen.phkafka;
 
 namespace PhPPTGen {
 	class Program {
-		//static void Main(string[] args) {
-		//	phSocket.PhThreadSocketServ s = new phSocket.PhThreadSocketServ();
-		//	s.startListen();
-		//	phCommon.PhMsgLst lst = phCommon.PhMsgLst.GetInstance();
-		//	lst.StartChecking();
-		//}
-
 		static void Main(string[] args) {
+			var msg = File.OpenText(@"D:\alfredyang\msg.txt").ReadToEnd();
+			PhConsumer.PushMsg(msg, "test");
 			PhConfigHandler.GetInstance().path = @"..\..\resources\";
 			phCommandFactory.PhCommandFactory.GetInstance().SeTmpDictionary(@"D:\pptresult\css\");
 			PhConfigHandler.GetInstance().init();
-
-			//using (SpreadsheetDocument mySpreadsheet = SpreadsheetDocument.Open(@"D:\alfredyang\aa6452e1-bf63-47ab-bdf2-b5e19a5200c7.xlsx", true)) {
-			//	//Get all the appropriate parts
-			//	WorkbookPart workbookPart = mySpreadsheet.WorkbookPart;
-
-			//	//生成chart在excel中
-			//	phOpenxml.PhExcelHandler.GetInstance().InsertChartIntoExcel(workbookPart, "Line");
-			//}
-			var input = new phModel.PhRequest() {
-				jobid = "dcsTest",
-				e2c = new phModel.PhExcel2Chart() {
-					name = "test4 - 副本 - 副本",
-					pos = new int[4] { (int)(254 / 0.000278), (int)(432 / 0.000278), (int)(1947 / 0.000278), (int)(1284 / 0.000278) },
-					chartType = "Column",
-					slider = 1
-				}
-
-				//e2c = new phModel.PhExcel2Chart() {
-				//	name = "test",
-				//	pos = new int[4] { (int)(169 / 0.000278), (int)(624 / 0.000278), (int)(927 / 0.000278), (int)(1105 / 0.000278) },
-				//	chartType = "Stacked",
-				//	slider = 1
-				//}
-			};
-			new phCommand.phChart.PhPPTImportChartCommand().Exec(input);
-		
+			phCommon.PhRequestLst lst = phCommon.PhRequestLst.GetInstance();
+			lst.StartChecking();
+			PhConsumer.PullMsg("test");
 		}
+
+		//static void Main(string[] args) {
+		//	PhConfigHandler.GetInstance().path = @"..\..\resources\";
+		//	phCommandFactory.PhCommandFactory.GetInstance().SeTmpDictionary(@"D:\pptresult\css\");
+		//	PhConfigHandler.GetInstance().init();
+
+		//	//using (SpreadsheetDocument mySpreadsheet = SpreadsheetDocument.Open(@"D:\alfredyang\aa6452e1-bf63-47ab-bdf2-b5e19a5200c7.xlsx", true)) {
+		//	//	//Get all the appropriate parts
+		//	//	WorkbookPart workbookPart = mySpreadsheet.WorkbookPart;
+
+		//	//	//生成chart在excel中
+		//	//	phOpenxml.PhExcelHandler.GetInstance().InsertChartIntoExcel(workbookPart, "Line");
+		//	//}
+		//	var input = new phModel.PhRequest() {
+		//		jobid = "dcsTest",
+		//		e2c = new phModel.PhExcel2Chart() {
+		//			name = "test4 - 副本 - 副本",
+		//			pos = new int[4] { (int)(254 / 0.000278), (int)(432 / 0.000278), (int)(1947 / 0.000278), (int)(1284 / 0.000278) },
+		//			chartType = "Column",
+		//			slider = 1
+		//		}
+
+		//		//e2c = new phModel.PhExcel2Chart() {
+		//		//	name = "test",
+		//		//	pos = new int[4] { (int)(169 / 0.000278), (int)(624 / 0.000278), (int)(927 / 0.000278), (int)(1105 / 0.000278) },
+		//		//	chartType = "Stacked",
+		//		//	slider = 1
+		//		//}
+		//	};
+		//	new phCommand.phChart.PhPPTImportChartCommand().Exec(input);
+
+		//}
 	}
 }
---
+++ PhPPTGen/phkafka/PhConsumer.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+using System.Threading.Tasks;
+using PhPPTGen.phModel;
+using Newtonsoft.Json;
+using JsonApiSerializer;
+
+namespace PhPPTGen.phkafka {
+	class PhConsumer {
+		//public static void Main(string[] args) {
+		//	PushMsg("test1", "test");
+		//	PushMsg("test2", "test");
+		//	var conf = new ConsumerConfig {
+		//		GroupId = "console-consumer-25919",
+		//		BootstrapServers = "59.110.31.50:9093",
+		//		// Note: The AutoOffsetReset property determines the start offset in the event
+		//		// there are not yet any committed offsets for the consumer group for the
+		//		// topic/partitions of interest. By default, offsets are committed
+		//		// automatically, so in this example, consumption will only start from the
+		//		// earliest message in the topic 'my-topic' the first time you run the program.
+		//		AutoOffsetReset = AutoOffsetReset.Earliest
+		//	};
+
+		//	using (var c = new ConsumerBuilder<Ignore, string>(conf).Build()) {
+		//		c.Subscribe("test");
+
+		//		CancellationTokenSource cts = new CancellationTokenSource();
+		//		Console.CancelKeyPress += (_, e) => {
+		//			e.Cancel = true; // prevent the process from terminating.
+		//			cts.Cancel();
+		//		};
+
+		//		try {
+		//			while (true) {
+		//				try {
+		//					var cr = c.Consume(cts.Token);
+		//					Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+		//				} catch (ConsumeException e) {
+		//					Console.WriteLine($"Error occured: {e.Error.Reason}");
+		//				}
+		//			}
+		//		} catch (OperationCanceledException) {
+		//			// Ensure the consumer leaves the group cleanly and final offsets are committed.
+		//			c.Close();
+		//		}
+		//	}
+		//}
+
+		public static async Task PushMsg(string msg, string topic) {
+			var config = new ProducerConfig {
+				BootstrapServers = "59.110.31.50:9092" 
+			};
+
+			// If serializers are not specified, default serializers from
+			// `Confluent.Kafka.Serializers` will be automatically used where
+			// available. Note: by default strings are encoded as UTF8.
+			using (var p = new ProducerBuilder<Null, string>(config).Build()) {
+				try {
+					var dr = await p.ProduceAsync(topic, new Message<Null, string> { Value = msg });
+					Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+				} catch (ProduceException<Null, string> e) {
+					Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+				}
+			}
+		}
+
+		public static void PullMsg(string topic) {
+			var conf = new ConsumerConfig {
+				GroupId = "console-consumer-25919",
+				BootstrapServers = "59.110.31.50:9093",
+				// Note: The AutoOffsetReset property determines the start offset in the event
+				// there are not yet any committed offsets for the consumer group for the
+				// topic/partitions of interest. By default, offsets are committed
+				// automatically, so in this example, consumption will only start from the
+				// earliest message in the topic 'my-topic' the first time you run the program.
+				AutoOffsetReset = AutoOffsetReset.Earliest
+			};
+
+			using (var c = new ConsumerBuilder<Ignore, string>(conf).Build()) {
+				c.Subscribe(topic);
+
+				CancellationTokenSource cts = new CancellationTokenSource();
+				Console.CancelKeyPress += (_, e) => {
+					e.Cancel = true; // prevent the process from terminating.
+					cts.Cancel();
+				};
+
+				try {
+					while (true) {
+						try {
+							var cr = c.Consume(cts.Token);
+							Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+							var msg = JsonConvert.DeserializeObject<PhRequest>(cr.Value, new JsonApiSerializerSettings());
+							phCommon.PhRequestLst.GetInstance().PushMsg(msg);
+						} catch (ConsumeException e) {
+							Console.WriteLine($"Error occured: {e.Error.Reason}");
+						}
+					}
+				} catch (OperationCanceledException) {
+					// Ensure the consumer leaves the group cleanly and final offsets are committed.
+					c.Close();
+				}
+			}
+		}
+	}
+}
